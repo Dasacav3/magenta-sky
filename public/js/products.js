a849252f0b39543d58bdec42e8910de3d5b4a88e
Dasacav3 <dscv3719@gmail.com>
2021-08-15 21:44:19 -0500

    Añadido ventanas modales

--- public/js/products.js
+++ public/js/products.js
@@ -1,43 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let btnModalAdd = document.getElementById("btnModalAdd");
-    btnModalAdd.addEventListener('click', openModal, false);
-    function openModal(){
-        Swal.fire({
-            title: "<strong>Añadir Producto</strong>",
-            html:
-                `
-                <div class="flex justify-center flex-col">
-                    <label>Nombre producto</label>
-                    <input type="text" class="border border-black">
-                    <label>Producto SKU</label>
-                    <input type="text" class="border border-black">
-                    <label>Precio</label>
-                    <input type="number" class="border border-black">
-                    <label>Stock</label>
-                    <input type="number" class="border border-black">
-                    <label>Producto SKU</label>
-                    <input type="text" class="border border-black">
-                    <label>Descripción corta</label>
-                    <textarea class="border border-black resize-none" rows="3" cols="25"></textarea>
-                    <label>Descripción tarjeta</label>
-                    <textarea class="border border-black resize-none" rows="3" cols="25"></textarea>
-                    <label>Descripción larga</label>
-                    <textarea class="border border-black resize-none" rows="10" cols="25"></textarea>
-                    <label>Categorias</label>
-                    <input type="text" class="border border-black" placeholder="Escribe para añadir una categoria">
-                    <button class="bg-purple-400 text-white w-max px-3 self-center my-3">Añadir categoria</button>
-                    <textarea class="border border-black resize-none" rows="10" cols="25" readonly></textarea>
-                    <label>Imagen</label>
-                    <input type="file" class="border border-black">
-                </div>
-                `,
-            showCloseButton: true,
-            showCancelButton: true,
-            focusConfirm: false,
-            confirmButtonText: 'Añadir',
-            confirmButtonAriaLabel: "Thumbs up, great!",
-            cancelButtonText: 'Cancelar',
-            cancelButtonAriaLabel: "Thumbs down",
-        });
+    // Modal add products
+    let containerFormAdd = document.getElementById("modalAdd");
+	let formAdd = document.getElementById("addProductForm");
+
+    let btnFormAddOpen = document.getElementById("btnModalOpenAdd");
+    btnFormAddOpen.addEventListener("click", openModalAdd, false);
+
+    let btnFormAddClose = document.getElementById("btnModalCloseAdd");
+    btnFormAddClose.addEventListener("click", closeModalAdd, false);
+
+    function openModalAdd() {
+        containerFormAdd.classList.add("show");
+        formAdd.classList.add("show");
+    }
+
+    function closeModalAdd() {
+        containerFormAdd.classList.remove("show");
+        formAdd.classList.remove("show");
     }
 });
